f68b8876d82963c7ba6752a17da6a09655f04c01
Gabibs <gabibsdev@gmail.com>
2022-05-29 22:25:28 -0300

    Exercícios dia 5.3

--- fundamentos/bloco-5-javascript-dom-eventos-web-storage/3-javascript-eventos/fixar/main.js
+++ fundamentos/bloco-5-javascript-dom-eventos-web-storage/3-javascript-eventos/fixar/main.js
@@ -13,7 +13,9 @@ const myWebpage = document.getElementById('my-spotrybefy');
 // 2.1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
 function addTech(event) {
   let techElement = document.querySelector('.tech');
-  techElement.classList.remove('tech');
+  if (techElement) {
+    techElement.classList.remove('tech');
+  }
   event.target.classList.add('tech');
   input.value = '';
 }
@@ -33,12 +35,24 @@ input.addEventListener('input', function(event) {
 // 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 // redirecione para alguma página;
 // 4.1. Que tal redirecionar para seu portfólio?
+function topSpotrybefy(event) {
+  window.location.replace('https://gabibsdev.github.io/');
+}
+
+myWebpage.addEventListener('dblclick', topSpotrybefy);
 
 // 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 // a cor do mesmo;
+myWebpage.addEventListener('mouseover', function(event) {
+  event.target.style.color = 'red';
+});
+
+myWebpage.addEventListener('mouseout', function(event) {
+  event.target.style.color = 'unset';
+});
+
 
 // Segue abaixo um exemplo do uso de event.target:
-
 
 function resetText(event) {
   // O Event é passado como um parâmetro para a função.
