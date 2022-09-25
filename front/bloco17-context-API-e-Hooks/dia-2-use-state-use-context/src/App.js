8f06510d4300ebffe8ee4ba63667015cf9f04438
Gabriella <gabibsdev@gmail.com>
2022-09-25 14:37:59 -0300

    Exercicios dia 17.2

---
+++ front/bloco17-context-API-e-Hooks/dia-2-use-state-use-context/src/App.js
@@ -0,0 +1,15 @@
+import Form from './components/Form';
+import StudentsInfo from './components/StudentsInfo';
+
+function App() {
+  return (
+    <>
+      <Form />
+      <StudentsInfo />
+    </>
+  );
+}
+
+//link do video de explicacao no final da pagina: https://app.betrybe.com/learn/course/5e938f69-6e32-43b3-9685-c936530fd326/module/095ebb0d-1932-4d37-933b-9e1d721646fb/section/94fad02a-cf1d-4277-871d-1553af1aded4/day/29da2fd9-c891-4b67-8d97-635c748f4e2a/lesson/2eae78c1-7095-4d59-b1c6-d455f9f4bc92/solution
+
+export default App;
